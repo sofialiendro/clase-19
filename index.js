8d0876b299b5ca2ea86baa42427f4d628c7d6d48
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-27 20:31:36 -0300

    Agrego código JS

---
+++ index.js
@@ -0,0 +1,49 @@
+const card = document.querySelector(".card")
+const img = document.querySelector("img")
+
+img.onclick = () => {
+  const corazon = document.querySelector(".fa.fa-heart")
+  corazon.classList.add("rojo")
+}
+
+card.onclick = (e) => {
+  console.log(e)
+}
+
+card.ondblclick = () => {
+    console.log("Hiciste doble clic.")
+  }
+
+
+window.onkeydown = (e) => {
+    console.log(e)
+    console.log(e.keyCode)
+    console.log("Hiciste key down.")
+}
+
+window.onkeypress = () => {
+    console.log("Hiciste key press.")
+}
+
+window.onkeyup = () => {
+    console.log("Hiciste key up.")
+}
+
+window.onresize = () => {
+    console.log("Cambiaste el tamaño de la pantalla.")
+}
+
+img.onmouseenter = () => {
+    console.log("Entra mouse.")
+}
+
+img.onmouseleave = () => {
+    console.log("Sale mouse.")
+}
+
+
+// window.onkeydown = (e) => {
+//   if (e.keyCode === 13) {
+//     alert("apretaste enter")
+//   }
+// }
